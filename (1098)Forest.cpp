6312b253169e90dc81cfc51bfef267e73d831ff5
ZSB <1948765057@qq.com>
2017-06-20 16:30:34 +0800

    Update (1098)Forest

--- (1098)Forest.cpp
+++ (1098)Forest.cpp
@@ -2,26 +2,59 @@
 #include<malloc.h>
 #include<algorithm>
 #include<math.h>
+#include<string.h>
 #define PR printf
 #define SC scanf
 using namespace std;
 int main()
 {
-	int total,i=0,test;
-	char num[1000];
+	int total;
 	SC("%d",&total);
+	char* str=(char*)malloc(total*sizeof(char)*10000);
+	int* ans=(char*)malloc(total*sizeof(int)*2);
 	for(int i=0;i<total;i++)
 	{
-		test=getchar();
-		while(test!='=')
-		{	
-			if(test=='*'&&test=='/'&&test=='-'&&test=='+')
+		scanf("%s",&str[i]);
+		int length=(int)strlen(&str[i]);
+		char number[10];
+		char symble[5000];
+		int numlist[5000];
+		int symnum=0;
+		int k=0;
+		for(int j=0;j<length;j++)
+		{
+			if(*(&str[i]+j)=='+'||*(&str[i]+j)=='-'||*(&str[i]+j)=='*'||*(&str[i]+j)=='/'||*(&str[i]+j)=='=')
 			{
-				putchar(test);
+				symble[symnum]=*(&str[i]+j);
+				sscanf(number,"%d",&numlist[symnum]);
+				symnum++;
+				for(int l=0;l<10;l++)
+				{
+					number[l]=NULL;
+				}
+				k=0;
+				continue;
 			}
-			num[i]=getchar();
-			i++;
-			num[i]='\0';
+			number[k]=*(&str[i]+j);
+			k++;	
+		}
+		/*for(int x=0;x<symnum;x++)
+		{
+			printf("%d\n",numlist[x]);
+			printf("%c\n",symble[x]);
+		}
+		printf("====================\n");*/
+		*(&ans[i])=numlist[0];
+		*(&ans[i]+1)=1;
+		for(int y=0;y<symnum-1;y++)
+		{
+			switch(symble[symnum])
+				case '+':
+					*(&ans[i])+=(*(&ans[i]+1))*symble[symnum+1];
+				case '-':
+					*(&ans[i])-=(*(&ans[i]+1))*symble[symnum+1];
+				case '*':
+					*(&ans[i])=(*(&ans[i]))*symble[symnum+1];
 		}
 	}
 	return 0;

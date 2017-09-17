9132824d947619d2a1f8c8670c5f83f22989b52f
ZSB <1948765057@qq.com>
2017-09-17 21:55:17 +0800

    Update (1174)TheLongestRoute.cpp

--- (1174)TheLongestRoute.cpp
+++ (1174)TheLongestRoute.cpp
@@ -3,62 +3,67 @@
 #include<algorithm>
 using namespace std;
 int final=0;
-int sum=0;
 int snum(int a,int b)
 {
 	if(a>=b)
-		return a-b;
+		return (a-b)*(a-b);
 	else
-		return b-a;
+		return (b-a)*(b-a);
 }
-int dfs(int first,int from,int num,int total,int* list)
+int sign(int number,int total,int* list)
 {
-	if(list[num]==1)
+	if(list[number]==1)
 	{
 		return 0;
 	}
-	int flag=1;
+	int flag=0;
 	for(int i=1;i<=total;i++)
 	{
-		if(list[i]=0)
+		if(list[i]==0)
 		{
-			flag=0;
+			flag=flag+1;
 		}
 	}
-	if(flag)
+	return flag;
+}
+void dfs(int first,int from,int num,int total,int sum,int* list)
+{
+	int flag=sign(num,total,list);
+
+	if(flag==0)
 	{
-		sum+=snum(first,num)*snum(first,num)+snum(from,num)*snum(from,num);
-		if(sum>final)
+		return;
+	}
+	if(flag==1)
+	{
+		if(final<(sum+snum(from,num)+snum(first,num)))
 		{
-			sum=sum^final;
-			final=final^sum;
-			sum=sum^final;
+			final=sum+snum(from,num)+snum(first,num);
 		}
-		sum=0;
-		return 1;
 	}
 	list[num]=1;
-	int cpsum=sum;
+	sum+=snum(from,num);
+	int* sumlist=(int*)malloc(total*sizeof(int));
 	for(int i=1;i<=total;i++)
 	{
-		if(dfs(first,num,i,total,list)!=0)
-		{
-			sum+=snum(i,num)*snum(i,num);
-		}
+		sumlist[i]=sum;
+		dfs(first,num,i,total,sumlist[i],list);
 	}
+	list[num]=0;
 }
 int main()
 {
 	int number;
 	scanf("%d",&number);
-	int* numlist=(int*)malloc((number+1)*sizeof(int));
+	int* list=(int*)malloc((number+1)*sizeof(int));
 	for(int i=1;i<=number;i++)
 	{
-		numlist[i]=0;
+		list[i]=0;
 	}
 	for(int i=1;i<=number;i++)
 	{
-		dfs(i,i,i,number,numlist);
+		int sum=0;
+		dfs(i,i,i,number,sum,list);
 	}
 	printf("%d",final);
 	return 0;
